d050c942734883c702400a1fd3abb5e45f6b6874
Joonatan Saarhelo <joon.saar@gmail.com>
2018-01-31 00:42:01 +0200

    add charset to html

--- app/Main.hs
+++ app/Main.hs
@@ -42,6 +42,7 @@ $doctype 5
     <head>
         <title>Regex Equivalence
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="utf-8">
 
     <body>
         <h1>Regex Equivalence
